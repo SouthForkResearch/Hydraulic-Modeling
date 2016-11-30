5491c645e1c14965dc4c78e5dfc7186addfff7d5
Matthew Nahorniak <matt@southforkresearch.org>
2016-11-30 14:15:46 -0800

    Added more plots to the BC copy, as are needed for high discharge models to check for flooding.  Also added file of trim-lengths, which will later be added to automatically set trim lengths by site based on previous needs for manual over-rides.  If a site isn't listed, use default = 2 m.

--- BC_jpg_copy.R
+++ BC_jpg_copy.R
@@ -11,32 +11,23 @@ if (site.list$Model[i] == "Yes") {
 if ("Boundary_Conditions.jpg" %in% files) {
 
 copyfile1=paste(site.list$Results.Folder[i], "//Boundary_Conditions.jpg", sep="")
-#copyfile2=paste(site.list$Results.Folder[i], "//Depth.Error.jpg", sep="")
+copyfile2=paste(site.list$Results.Folder[i], "//Depth.Error.jpg", sep="")
+copyfile3=paste(site.list$Results.Folder[i], "//Velocity.Magnitude.jpg", sep="")
+copyfile4=paste(site.list$Results.Folder[i], "//Depth.jpg", sep="")
+
 
 pastefile1 = paste(BC_Dir,"//Boundary_Conditions_",i,".jpg", sep="")
-#pastefile2 = paste(BC_Dir,"//Depth_Error_",i,".jpg", sep="")
+pastefile2 = paste(BC_Dir,"//Depth_Error_",i,".jpg", sep="")
+pastefile3 =  paste(BC_Dir,"//Velocity_Magnitude_",i,".jpg", sep="")
+pastefile4 =  paste(BC_Dir,"//Depth_",i,".jpg", sep="")
+
 
 file.copy(copyfile1, pastefile1, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
-#file.copy(copyfile2, pastefile2, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
-}
-}
-}
-
-
-
-for (i in 1:length(site.list$Results.Folder)){
-files=dir(site.list$Results.Folder[i])
-if (site.list$Model[i] == "Yes") {
-if ("Depth.Error.jpg" %in% files) {
-
-#copyfile1=paste(site.list$Results.Folder[i], "//Boundary_Conditions.jpg", sep="")
-copyfile2=paste(site.list$Results.Folder[i], "//Depth.Error.jpg", sep="")
-
-#pastefile1 = paste(BC_Dir,"//Boundary_Conditions_",i,".jpg", sep="")
-pastefile2 = paste(BC_Dir,"//Depth_Error_",i,".jpg", sep="")
-
-#file.copy(copyfile1, pastefile1, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
 file.copy(copyfile2, pastefile2, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
+file.copy(copyfile3, pastefile3, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
+file.copy(copyfile4, pastefile4, overwrite=T, copy.mode = TRUE, copy.date = FALSE)
+
 }
 }
 }
+
